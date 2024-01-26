f6b9fbc282545881aaafea26bbb772c975f0f45f
Muhammad Ali Hasan <alihxn23@gmail.com>
2024-01-26 17:14:57 -0600

    added tests to pipeline

--- lib/pipeline-stack.ts
+++ lib/pipeline-stack.ts
@@ -30,5 +30,29 @@ export class WorkshopPipelineStack extends Stack {
 
         const deploy = new WorkshopPipelineStage(this, 'Deploy')
         const deployStage = pipeline.addStage(deploy)
+
+        deployStage.addPost(
+            new CodeBuildStep("TestViewerEndpoint", {
+                projectName: 'TestViewerEndpoint',
+                envFromCfnOutputs: {
+                    ENDPOINT_URL: deploy.hcViewerUrl
+                },
+                commands: [
+                    'curl -Ssf $ENDPOINT_URL'
+                ]
+            }),
+
+            new CodeBuildStep("TestApiGatewayEndpoint", {
+                projectName: 'TestApiGatewayEndpoint',
+                envFromCfnOutputs: {
+                    ENDPOINT_URL: deploy.hcEndpoint
+                },
+                commands: [
+                    'curl -Ssf $ENDPOINT_URL',
+                    'curl -Ssf $ENDPOINT_URL/hello',
+                    'curl -Ssf $ENDPOINT_URL/test'
+                ]
+            })
+        )
     }
 }
--- lib/pipeline-stage.ts
+++ lib/pipeline-stage.ts
@@ -1,11 +1,17 @@
-import { StackProps, Stage } from "aws-cdk-lib";
+import { CfnOutput, StackProps, Stage } from "aws-cdk-lib";
 import { Construct } from "constructs";
 import { CdkWorkshopStack } from "./cdk-workshop-stack";
 
 export class WorkshopPipelineStage extends Stage {
+    public readonly hcViewerUrl: CfnOutput;
+    public readonly hcEndpoint: CfnOutput
+
     constructor(scope: Construct, id: string, props?: StackProps) {
         super(scope, id, props)
 
-        new CdkWorkshopStack(this, 'WebService')
+        const service = new CdkWorkshopStack(this, 'WebService')
+
+        this.hcViewerUrl = service.hcViewerUrl
+        this.hcEndpoint = service.hcEndpoint
     }
 }
