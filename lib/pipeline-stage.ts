5381bc801c1c4f1fb9e82f21bfb9bf94c75496bf
Muhammad Ali Hasan <alihxn23@gmail.com>
2024-01-26 16:56:35 -0600

    add deploy to pipeline

---
+++ lib/pipeline-stage.ts
@@ -0,0 +1,11 @@
+import { StackProps, Stage } from "aws-cdk-lib";
+import { Construct } from "constructs";
+import { CdkWorkshopStack } from "./cdk-workshop-stack";
+
+export class WorkshopPipelineStage extends Stage {
+    constructor(scope: Construct, id: string, props?: StackProps) {
+        super(scope, id, props)
+
+        new CdkWorkshopStack(this, 'WebService')
+    }
+}
